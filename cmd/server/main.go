5af177a00e94c9e4512348ec2e550935f4ce6fb0
anton.baranov@waveaccess.global <anton.baranov@waveaccess.global>
2023-04-04 17:13:16 +0300

    changed memstor

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,147 +15,131 @@ func main() {
 }
 
 func run() error {
-	storage := &MemStorage{metrics: make(map[string]Metric)}
+	storage := NewMemStorage()
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/update/", storage.updateMetricHandler)
-	mux.HandleFunc("/show/", storage.ShowHandler)
+	mux.HandleFunc("/update/", storage.updateMetricsHandler)
+	mux.HandleFunc("/show/", storage.showAllHandler)
 	return http.ListenAndServe("0.0.0.0:8080", mux)
 }
 
 type MemStorage struct {
-	metrics map[string]Metric
+	// FIXME Нужен ли дополнительный словарь чтобы хранить название и тип существующих метрик? Или просто искать имя в обеих мапах?
+	metrics     map[string]string
+	gaugeStor   map[string]float64
+	counterStor map[string]int64
 }
 
-func (s *MemStorage) updateMetricHandler(w http.ResponseWriter, r *http.Request) {
+func NewMemStorage() *MemStorage {
+	return &MemStorage{
+		metrics:     make(map[string]string),
+		gaugeStor:   make(map[string]float64),
+		counterStor: make(map[string]int64),
+	}
+}
+
+// FIXME Привязывать ли хендлер к структуре или сделать вызов функции которая обернет логику в хендлер?
+func (s *MemStorage) updateMetricsHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received request with url: %v", r.URL.String())
 	if r.Method != http.MethodPost {
 		http.Error(w, "Only POST request is allowed.", http.StatusMethodNotAllowed)
 		return
 	}
-
-	metric, err := parsePath(r.URL.Path)
+	metric, err := parseURL(r.URL.Path)
 	if err != nil {
+		log.Printf("Error while parsing url")
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
-	s.updateMetric(metric)
-	log.Printf("Metric updated. Current storage: %v", s)
+	if err := s.updateMetric(metric); err != nil {
+		log.Printf("Error while updating metric")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 }
 
-func (s *MemStorage) updateMetric(m Metric) {
-	log.Printf("Updating storage with metric: %v, value: %v", m.GetName(), m.GetValue())
-	m.Update(s)
+func (s *MemStorage) updateMetric(m *RawMetric) error {
+	log.Printf("Updating storage with metric: %v, type: %v, value: %v", m.Name, m.mType, m.Value)
+	switch m.mType {
+	case "gauge":
+		return s.updateGauge(m)
+	case "counter":
+		return s.updateCounter(m)
+	default:
+		log.Printf("Error. Invalid metric type")
+		return errors.New("bad request: invalid metric type")
+	}
 }
 
-func (s *MemStorage) ShowHandler(w http.ResponseWriter, r *http.Request) {
-	log.Print("Printing storage content: ")
+func (s *MemStorage) updateGauge(m *RawMetric) error {
+	parsedValue, err := strconv.ParseFloat(m.Value, 64)
+	if err != nil {
+		return errors.New("bad request: wrong value")
+	}
+	log.Printf("Parsed value of %v is %v", m.Value, parsedValue)
+
+	log.Printf("Checking if metrics exists")
+	existingMetric, ok := s.metrics[m.Name]
+	if ok && existingMetric != m.mType {
+		return errors.New("bad request: metric with same name and different type already exists")
+	} else if !ok {
+		s.metrics[m.Name] = m.mType
+	}
+
+	log.Printf("Storing new value")
+	s.gaugeStor[m.Name] = parsedValue
+	log.Printf("%v is stored in gauge storage. Current values is %v", m.Name, s.gaugeStor[m.Name])
+	return nil
+}
+
+func (s *MemStorage) updateCounter(m *RawMetric) error {
+	parsedValue, err := strconv.ParseInt(m.Value, 10, 64)
+	if err != nil {
+		return errors.New("bad request: wrong value")
+	}
+	log.Printf("Parsed value of %v is %v", m.Value, parsedValue)
+
+	log.Printf("Checking if metrics exists")
+	existingMetric, ok := s.metrics[m.Name]
+	if ok && existingMetric != m.mType {
+		return errors.New("bad request: metric with same name and different type already exists")
+	} else if !ok {
+		s.metrics[m.Name] = m.mType
+	}
+
+	s.counterStor[m.Name] += parsedValue
+	log.Printf("%v is stored in counter storage. Current value is %v", m.Name, s.counterStor[m.Name])
+	return nil
+}
+
+func (s *MemStorage) showAllHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Registred metrics: ")
 	for k, v := range s.metrics {
-		log.Printf("metrics[%v] -> %v = %v", k, v.GetName(), v.GetValue())
+		log.Printf("%v type is %v", k, v)
+		switch v {
+		case "gauge":
+			log.Printf("Value is %v", s.gaugeStor[k])
+		case "counter":
+			log.Printf("Value is %v", s.counterStor[k])
+		}
 	}
 }
 
-func parsePath(path string) (Metric, error) {
-	var metric Metric
+// FIXME Как лучше вернуть три значения? Выносить ли в отдельную функцию? Привязывать ли функцию к структуре, она вроде связанна логически, но данные структуры не нужны?
+func parseURL(path string) (*RawMetric, error) {
 	p := strings.Split(path, "/")
 	if len(p) != 5 {
 		return nil, errors.New("bad request: malformed URL")
 	}
-
-	rawType := p[2]
-	rawName := p[3]
-	rawValue := p[4]
-
-	switch rawType {
-	case "gauge":
-		metric = &Gauge{}
-	case "counter":
-		metric = &Counter{}
-	default:
-		return nil, errors.New("bad request: invalid metric type")
-	}
-	if err := metric.SetName(rawName); err != nil {
-		return nil, err
-	}
-	if err := metric.SetValue(rawValue); err != nil {
-		return nil, err
-	}
-	return metric, nil
+	return &RawMetric{
+		mType: p[2],
+		Name:  p[3],
+		Value: p[4],
+	}, nil
 }
 
-type Metric interface {
-	SetName(string) error
-	SetValue(string) error
-	GetName() string
-	GetValue() string
-	Update(s *MemStorage)
-}
-
-type Gauge struct {
+type RawMetric struct {
+	mType string
 	Name  string
-	Value float64
-}
-
-func (g *Gauge) SetName(s string) error {
-	g.Name = s
-	return nil
-}
-
-func (g *Gauge) SetValue(s string) error {
-	value, err := strconv.ParseFloat(s, 64)
-	if err != nil {
-		return errors.New("bad request: wrong value")
-	}
-	g.Value = value
-	return nil
-}
-
-func (g *Gauge) GetName() string {
-	return g.Name
-}
-
-func (g *Gauge) GetValue() string {
-	return strconv.FormatFloat(g.Value, 'e', 2, 64)
-}
-
-func (g *Gauge) Update(s *MemStorage) {
-	s.metrics[g.Name] = g
-}
-
-type Counter struct {
-	Name  string
-	Value int64
-}
-
-func (c *Counter) SetName(s string) error {
-	c.Name = s
-	return nil
-}
-
-func (c *Counter) Update(s *MemStorage) {
-	if _, ok := s.metrics[c.Name]; ok {
-		storValue, err := strconv.ParseInt(s.metrics[c.Name].GetValue(), 10, 64)
-		// FIXME
-		if err != nil {
-			return
-		}
-		c.Value += storValue
-	}
-	s.metrics[c.Name] = c
-}
-
-func (c *Counter) SetValue(s string) error {
-	value, err := strconv.ParseInt(s, 10, 64)
-	if err != nil {
-		return errors.New("bad request: wrong value")
-	}
-	c.Value = value
-	return nil
-}
-
-func (c *Counter) GetName() string {
-	return c.Name
-}
-
-func (c *Counter) GetValue() string {
-	return strconv.FormatInt(c.Value, 10)
+	Value string
 }
