e9ae4d07dcf8229e85f4b07afc91e1298462723a
anton.baranov@waveaccess.global <anton.baranov@waveaccess.global>
2023-04-10 19:57:54 +0300

    ignore unknown flags for agent

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 )
 
 var (
@@ -11,9 +12,11 @@ var (
 )
 
 func parseFlags() {
-	flag.StringVar(&flagServerAddress, "a", "localhost:8080", "server address and port to listen")
-	flag.IntVar(&flagReportInterval, "r", 10, "report interval in seconds")
-	flag.IntVar(&flagPollInterval, "p", 2, "poll interval in seconds")
+	flagSet := flag.NewFlagSet("main", flag.ContinueOnError)
 
-	flag.Parse()
+	flagSet.StringVar(&flagServerAddress, "a", "localhost:8080", "server address and port to listen")
+	flagSet.IntVar(&flagReportInterval, "r", 10, "report interval in seconds")
+	flagSet.IntVar(&flagPollInterval, "p", 2, "poll interval in seconds")
+
+	flagSet.Parse(os.Args)
 }
