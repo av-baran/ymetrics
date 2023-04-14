160cb1fe4f82be2856f4fdf9f92b9f841d5f0b46
anton.baranov@waveaccess.global <anton.baranov@waveaccess.global>
2023-04-14 17:00:28 +0300

    implemented http page with all metrics

--- internal/delivery/httphandlers/getall.go
+++ internal/delivery/httphandlers/getall.go
@@ -1,10 +1,23 @@
 package httphandlers
 
 import (
+	"fmt"
 	"net/http"
+)
+
+const (
+	pageHeader = `<html><body><h1>Metric list</h1><ul>`
+	pageFooter = `</ul></body></html>`
 )
 
 func GetAllMetricsHandler(g metricGetter) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		allMetrics := g.GetAllMetrics()
+		fmt.Fprint(w, pageHeader)
+		for k, v := range allMetrics {
+			fmt.Fprintf(w, "<li> %v = %v </li>", k, v)
+		}
+		fmt.Fprint(w, pageFooter)
 	}
 }
