2d4255327ec3f453d94e758c090622b9da01e9e6
anton.baranov@waveaccess.global <anton.baranov@waveaccess.global>
2023-04-21 01:12:01 +0300

    added memstor sync mutex

--- internal/repository/memstor/memstor.go
+++ internal/repository/memstor/memstor.go
@@ -3,6 +3,7 @@ package memstor
 import (
 	"errors"
 	"fmt"
+	"sync"
 
 	"github.com/av-baran/ymetrics/pkg/interrors"
 )
@@ -12,6 +13,8 @@ type MemStorage struct {
 	CounterStor map[string]int64
 }
 
+var memStorageSync = sync.Mutex{}
+
 func New() *MemStorage {
 	return &MemStorage{
 		GaugeStor:   make(map[string]float64),
@@ -20,15 +23,21 @@ func New() *MemStorage {
 }
 
 func (s *MemStorage) SetGauge(name string, value float64) {
+	memStorageSync.Lock()
+	defer memStorageSync.Unlock()
 	s.GaugeStor[name] = value
 }
 
 func (s *MemStorage) AddCounter(name string, value int64) {
+	memStorageSync.Lock()
+	defer memStorageSync.Unlock()
 	v := s.CounterStor[name]
 	s.CounterStor[name] = v + value
 }
 
 func (s *MemStorage) GetGauge(name string) (string, error) {
+	memStorageSync.Lock()
+	defer memStorageSync.Unlock()
 	v, ok := s.GaugeStor[name]
 	if !ok {
 		return "", errors.New(interrors.ErrMetricNotFound)
@@ -37,6 +46,8 @@ func (s *MemStorage) GetGauge(name string) (string, error) {
 }
 
 func (s *MemStorage) GetCounter(name string) (string, error) {
+	memStorageSync.Lock()
+	defer memStorageSync.Unlock()
 	v, ok := s.CounterStor[name]
 	if !ok {
 		return "", errors.New(interrors.ErrMetricNotFound)
@@ -45,9 +56,13 @@ func (s *MemStorage) GetCounter(name string) (string, error) {
 }
 
 func (s *MemStorage) GetAllGauge() map[string]float64 {
+	memStorageSync.Lock()
+	defer memStorageSync.Unlock()
 	return s.GaugeStor
 }
 
 func (s *MemStorage) GetAllCounter() map[string]int64 {
+	memStorageSync.Lock()
+	defer memStorageSync.Unlock()
 	return s.CounterStor
 }
